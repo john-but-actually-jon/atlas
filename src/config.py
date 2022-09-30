dd157239b59fe63918ce1737b2178a0bbb0638b6
Meridian-Onset <jma64wright@gmail.com>
2022-09-30 14:25:32 +0200

    :wrench: Added central python config file

---
+++ src/config.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+from logging import getLogger, INFO
+
+from exceptions import InvalidConfigException
+
+logger = getLogger()
+logger.setLevel(INFO)
+
+load_dotenv()
+
+try:
+    assert (CREDENTIALS := str(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")))
+except AssertionError:
+    logger.error("Local path to credentials not defined!")
+    raise InvalidConfigException
+SCOPES = {"gmail": ["https://www.googleapis.com/auth/gmail.readonly"]}
+
+
+if __name__ == "__main__":
+    print(CREDENTIALS)
