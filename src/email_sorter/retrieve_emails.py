815510c09519dfc9e0769fae44436634f178b47d
Meridian-Onset <jma64wright@gmail.com>
2022-10-09 18:13:14 +0200

    ✨ Added keyword extraction in EmailClassifier with unified configs

--- src/email_sorter/retrieve_emails.py
+++ src/email_sorter/retrieve_emails.py
@@ -1,9 +1,18 @@
-from dataclasses import dataclass
+import os
+from dataclasses import dataclass, field
 from src import config as cfg
-from src.generic_google_api_service import google_api_service
+from src.config import config_load
 from googleapiclient.discovery import Resource
-
+from email_class import Email
+import json
+from pathlib import Path
 from typing import List, Dict, Any, Literal, Optional
+
+from keybert import KeyBERT
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words("english"))
+
 
 logger = cfg.logger
 credentials_path = cfg.CREDENTIALS
@@ -93,9 +102,121 @@ class EmailRetriever:
         return emails
 
 
+@dataclass
+class EmailClassifier:
+    """
+    Class that makes classifying emails by importance for training, test and
+    validation data easier.
+
+    Initial implementation uses the command line to output information in the
+    email to the command line, then prompting the user to classify the email's
+    importance on a scale from 0 to 4.
+
+    Then it writes the emails along with there classification to a file in
+    the datasets folder
+
+    Parameters:
+        - `emails` (required): A list of parsed `Email` objects, from which
+        important information can be extracted.
+        - `file_prefix` (required): The prefix of the file that the labelled
+        email data is written to.
+        - `file_type` (optional): Desired output file format. One of ["csv",
+        "json"]. Default: "csv"
+    """
+
+    emails: List[Email]
+    file_prefix: str
+    file_type: Literal["csv", "json"] = "csv"
+    dataset_path: str = os.path.join(os.getcwd(), "datasets")
+    _processed_emails: List[Email] = field(default_factory=lambda: [])
+    _file_name: str = "labelled-emails"
+    keyword_extractor: KeyBERT = KeyBERT(model="all-mpnet-base-v2")
+    config = config_load(Path(Path(__file__).parent, "config.yaml"))
+
+    def _check_for_processed_emails(self) -> Optional[List[Email]]:
+        """
+        Check the datasets directory for emails that have already been classified
+
+        Returns:
+            - `_processed_emails`: Used in the `start_classification` call to
+            filter out the emails from the dataset in question that have
+            already been processed
+        """
+        file_name = os.path.join(
+            os.getcwd(), ".datasets", self._file_name, self.file_type
+        )
+        if os.path.isfile(file_name):
+            with open(file_name) as f:
+                emails_with_labels = json.load(f)
+            return emails_with_labels
+        else:
+            return None
+
+    def summarize_email(self, email: Email) -> Dict[str, Any]:
+        """
+        Output a list of important features from an email, including the
+        sender, receiver, date and the keywords from the email body, extracted
+        using the nltk library
+
+        Parameters:
+            email (required): The email to be summarized
+
+        Returns:
+            returnValue: Returns a summary of the important information in an
+            email. Which can be printed to the command line to assist with
+            labelling
+        """
+        features = {}
+        features["keywords"] = keyword_model.extract_keywords(
+            email.body,
+            stop_words="english",
+            highlight=config.highlight,
+            top_n=config.top_n,
+            keyphrase_ngram_range=(config.n_gram_bottom, config.n_gram_top),
+        )
+
+        return features
+
+    def start_classification(self, number_of_emails: Optional[int]) -> None:
+        """
+        Start the command line email classification process.
+
+        Parameters:
+            - `number_of_emails`: The number of emails to start with
+
+        Returns:
+            returnValue: This is a description of the returnValue
+        """
+        pass
+
+
 if __name__ == "__main__":
-    with google_api_service(
-        credentials_path, "gmail", "v1", scopes, pickle_prefix="mail-client"
-    ) as gmail:
-        retriever = EmailRetriever(gmail_service=gmail, number_of_emails=50)
-        print(retriever.fetch_email_ids())
+    # with google_api_service(
+    #     credentials_path, "gmail", "v1", scopes, pickle_prefix="mail-client"
+    # ) as gmail:
+    #     retriever = EmailRetriever(gmail_service=gmail, number_of_emails=50)
+    #     print(retriever.fetch_email_ids())
+    from src.config import config_load
+    from pathlib import Path
+
+    confpath = Path(Path(__file__).parent, "config.yaml")
+    print(confpath)
+    config = config_load(confpath).email_ranking.keybert_params
+
+    print(config)
+
+    with open(
+        Path(Path(__file__).parent.parent.parent, ".datasets", "labelled_emails.json"),
+        "r",
+    ) as f:
+        emails_with_labels = json.load(f)
+        print(emails_with_labels[0])
+    keyword_model = KeyBERT(model="all-mpnet-base-v2")
+    keywords = keyword_model.extract_keywords(
+        emails_with_labels[0]["body"],
+        stop_words="english",
+        highlight=config.highlight,
+        top_n=config.top_n,
+        keyphrase_ngram_range=(config.n_gram_bottom, config.n_gram_top),
+    )
+    print(keywords)
